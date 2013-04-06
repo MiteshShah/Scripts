f6d1f316e59e2a3df1605b278ec04036813a8acf
Mitesh Shah <Mitesh.Shah@rtcamp.com>
2013-04-06 13:33:11 +0530

    Fix Hostname

--- monitor-ssh-keys.sh
+++ monitor-ssh-keys.sh
@@ -8,6 +8,6 @@ do
         inotifywait --exclude .swp -e modify /root/.ssh/authorized_keys /home/*/.ssh/authorized_keys
 	echo "SSH Authorised Keys Files Modified At [`date`]" &>> /var/log/rtsecure.log
 	
-        echo "SSH Authorised Keys Files Modified At `date`" | mail -s "rtCamp.com SSH Keys Modified" Mitesh.Shah@rtcamp.com 
+        echo "SSH Authorised Keys Files Modified At `date`" | mail -s "$(hostname) SSH Keys Modified" Mitesh.Shah@rtcamp.com 
 done
 
