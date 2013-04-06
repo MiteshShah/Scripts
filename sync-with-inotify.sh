1341b4338027c4c766322539d594e22758042177
Mitesh Shah <Mitesh.Shah@rtcamp.com>
2013-04-06 14:43:31 +0530

    Mirror Webroot

--- sync-with-inotify.sh
+++ sync-with-inotify.sh
@@ -2,6 +2,6 @@
 while true
 do
 	inotifywait --exclude .swp ~  -r -e create -e modify -e delete -e move -e attrib \
-	--format %e:%f /var/www/mitesh.com/htdocs/
-	rsync -avz /var/www/mitesh.com/htdocs/ root@192.168.0.206:/var/www/mitesh.com/htdocs/
+	--format %e:%f /var/www/mitesh.com/
+	rsync -avz /var/www/mitesh.com/ root@192.168.0.206:/var/www/mitesh.com/
 done
