c464a388a79b5f6ca258561232d3d1def1b0acd9
Mitesh Shah <Mitesh.Shah@rtcamp.com>
2013-04-13 11:58:06 +0530

    More Better Way To Mirror

--- sync-with-inotify.sh
+++ sync-with-inotify.sh
@@ -1,24 +1,45 @@
 #!/bin/bash
 while true
 do
+
 	# Monitor Files Changes For Create, Delete, Move, File Permissions
         inotifywait --exclude .swp ~  -r -e create -e modify -e delete -e move -e attrib --format %e:%f /var/www/
 
-	# Rsync When Files Changed
-	rsync -avz /var/www/ root@LIVE-SERVER-IP:/var/www/
-        #rsync -avz --delete /var/www/ root@BACKUP-SERVER-IP:/var/www/
+	# Detect Server:
+	# Detect.txt is soft linked to /usr/local/bin/Detect.txt
+	# Detect.txt Contains Uniq Live/Backup Server Identify Strings
+	#curl -s DOMAIN.com/Detect.txt | grep BACKUPSERVERNAME
+	curl -s DOMAIN.com/Detect.txt | grep LIVESERVERNAME
 	
-	# If Rsync Fails
-	if [ $? != 0 ]
+
+	if [ $? -eq 0 ]
 	then
 
-		echo "[+] Checking Server Health Script Is Already Running Or Not ....."
-		ps ax | grep check-server-health.sh | grep -v grep
+		# Send Details To Log Files
+		echo "The LIVE-SERVER Is Running, Sending Changes From LIVE-SERVER To BACKUP-SERVER:" \
+		>> /var/log/sync-with-inotify.log
 
+
+		# Rsync When Files Changed
+		rsync -avz /var/www/ root@LIVE-SERVER-IP:/var/www/
+	        #rsync -avz --delete /var/www/ root@BACKUP-SERVER-IP:/var/www/
+	
+		# If Rsync Fails
 		if [ $? != 0 ]
 		then
-			echo "[+] Starting Check Server Health Script ....."
-			bash /root/bin/check-server-health.sh &
+
+			echo "[+] Checking Server Health Script Is Already Running Or Not ....."
+			ps ax | grep check-server-health.sh | grep -v grep
+
+			if [ $? != 0 ]
+			then
+				echo "[+] Starting Check Server Health Script ....."
+				bash /root/bin/check-server-health.sh &
+			fi
 		fi
+	else
+		# Send Details To Log Files
+		echo "The BACKUP-SERVER Is Running, Can't Sending Changes From BACKUP-SERVER To LIVE-SERVER:" \
+		>> /var/log/sync-with-inotify.log
 	fi
 done
