33d0b71d248083fc4729e179a5c1b16d1934f5fd
Mitesh Shah <Mitesh.Shah@rtcamp.com>
2013-05-16 12:48:07 +0530

    First Character Is Capital in Whoami

--- welcome.sh
+++ welcome.sh
@@ -10,22 +10,22 @@ TIME=$(date +%H)
 if [ $TIME -gt 05 ] && [ $TIME -lt 12 ]
 then
 	espeak -ven-us+f4 -s170 "Good Morning $(whoami)" &> /dev/null
-	notify-send "Good Morning $(whoami)" 2> /dev/null
+	notify-send "Good Morning $(whoami | sed -r 's/\<./\U&/g')" 2> /dev/null
 
 #From 12:00:00 to 16:59:59 Good Afternoon
 elif [ $TIME -gt 11 ] && [ $TIME -lt 17 ]
 then
 	espeak -ven-us+f4 -s170 "Good Afternoon $(whoami)" &> /dev/null
-	notify-send "Good Afternoon $(whoami)" 2> /dev/null
+	notify-send "Good Afternoon $(whoami | sed -r 's/\<./\U&/g')" 2> /dev/null
 
 #From 17:00:00 to 19:59:59 Good Evening
 elif [ $TIME -gt 16 ] && [ $TIME -lt 20 ]
 then
 	espeak -ven-us+f4 -s170 "Good Evening $(whoami)" &> /dev/null
-	notify-send "Good Evening $(whoami)" 2> /dev/null
+	notify-send "Good Evening $(whoami | sed -r 's/\<./\U&/g')" 2> /dev/null
 
 #From 20:00:00 to 05:59:59
 else
 	espeak -ven-us+f4 -s170 "Welcome $(whoami)" &> /dev/null
-	notify-send "Welcome $(whoami)" 2> /dev/null
+	notify-send "Welcome $(whoami | sed -r 's/\<./\U&/g')" 2> /dev/null
 fi
