73ae0d0806329ab864f3441e113344de13153a44
Mitesh Shah <Mitesh.Shah@rtcamp.com>
2013-07-16 12:21:51 +0530

    Updated Scripts

--- Gmail.sh
+++ Gmail.sh
@@ -6,15 +6,20 @@
 
 GMAILUSER=GmailUserName
 GMAILPASS='GmailPasswprd'
-echo $GMAILPASS
 
 curl -u $GMAILUSER:$GMAILPASS https://mail.google.com/mail/feed/atom > /tmp/GmailChecker
 COUNT=`cat /tmp/GmailChecker | grep fullcount | cut -d'>' -f2 | cut -d '<' -f1`
 
-for i in `seq 1 $COUNT`
-do
-	TITLE=`cat /tmp/GmailChecker | grep -v 'Gmail - Inbox' | grep title | sed -n $i\p | cut -d'>' -f2 | cut -d '<' -f1`
-	SUMMARY=`cat /tmp/GmailChecker | grep summary | sed -n $i\p | cut -d'>' -f2 | cut -d '<' -f1`
-	echo $TITLE
-	notify-send --icon kmail "($i/$COUNT) $TITLE <br><br> $SUMMARY"
-done
+if [ "$COUNT" -ge "1" ]
+then
+	for i in `seq 1 $COUNT`
+	do
+		TITLE=`cat /tmp/GmailChecker | grep -v 'Gmail - Inbox' | grep title | sed -n \
+		$i\p | cut -d'>' -f2 | cut -d '<' -f1`
+		SUMMARY=`cat /tmp/GmailChecker | grep summary | sed -n $i\p | cut -d'>' -f2 \
+		| cut -d '<' -f1`
+		notify-send --icon kmail "($i/$COUNT) $TITLE <br><br> $SUMMARY"
+	done
+else
+	notify-send --icon kmail "No New Mails"
+fi
