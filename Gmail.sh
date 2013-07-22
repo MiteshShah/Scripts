56b593a81af41ff1554cb96e49eedde7061eb322
Mitesh Shah <Mitesh.Shah@rtcamp.com>
2013-07-22 13:31:20 +0530

    Better Notification

--- Gmail.sh
+++ Gmail.sh
@@ -18,7 +18,7 @@ then
 		$i\p | cut -d'>' -f2 | cut -d '<' -f1`
 		SUMMARY=`cat /tmp/GmailChecker | grep summary | sed -n $i\p | cut -d'>' -f2 \
 		| cut -d '<' -f1`
-		notify-send --icon kmail "($i/$COUNT) $TITLE <br><br> $SUMMARY"
+		notify-send --icon kmail "($i/$COUNT)" "$TITLE <br> $SUMMARY"
 		sleep 5s;
 	done
 else
