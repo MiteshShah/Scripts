960cca19f695eb8dcb5b7c91be455ba16d9a64e2
Mitesh Shah <Mitesh.Shah@rtcamp.com>
2013-08-24 13:58:59 +0530

    Import MySQL Databases

--- mysql-import.sh
+++ mysql-import.sh
@@ -1,11 +1,34 @@
 #!/bin/bash
-for i in /var/rsnap-mysql/*
+
+
+
+# Capture Errors
+OwnError()
+{
+        echo -e "[ `date` ] \033[31m $@ \e[0m" | tee -ai $ERRORLOG
+        exit 101 
+}
+
+apt-get -y install pv || OwnError "Unable To Install PV"
+
+
+read -p "Database Pathe (/var/rsnap-mysql): " DBPATH
+
+# IF Enter Is Pressed
+if [[ $DBPATH = "" ]]
+then
+        DBPATH="/var/rsnap-mysql"
+        echo $DBPATH
+fi
+
+for i in $DBPATH/*
 do
-        echo
+        echo 
         echo
         read -p "Select Database For `basename $i`: " DB_NAME
         echo DBNAME = $DB_NAME 
         echo SQLFILE = $i
+        mysql -e "create database `$DBNAME`" || OwnError  "Unable To Create Database $DBANME"
         pv $i | mysql $DB_NAME
 done
 
