ddfcde4d06f0553f3cff80968a424a0f0f234446
Manish Songirkar <manish.songirkar@rtcamp.com>
2013-09-24 13:58:45 +0530

    Update eersync.sh

--- eersync.sh
+++ eersync.sh
@@ -29,14 +29,16 @@ then
 	read -p " Enter Source Domain Name To rsync: " MIGSRCDOMAIN
 fi
 
+MIGSRCCONFIG=/var/www/$MIGSRCDOMAIN/wp-config.php
+
 # Check If wp-config.php file exist or not
-if [ -f /var/www/$MIGSRCDOMAIN/wp-config.php ]
+if [ -f $MIGSRCCONFIG ]
 then
 	MIGDBEXPORT=y
 elif [ -f /var/www/$MIGSRCDOMAIN/htdocs/wp-config.php ]
 then
 	MIGDBEXPORT=y
-	mv -v /var/www/$MIGSRCDOMAIN/htdocs/wp-config.php /var/www/$MIGSRCDOMAIN/wp-config.php
+	MIGSRCCONFIG=/var/www/$MIGSRCDOMAIN/htdocs/wp-config.php
 else
 	MIGDBEXPORT=n
 fi
@@ -44,9 +46,9 @@ fi
 if [ "$MIGDBEXPORT" == "y" ]
 then
 	# MySQL Informatiom
-	MIGSRCDBNAME=$(grep DB_NAME /var/www/$MIGSRCDOMAIN/wp-config.php | cut -d"'" -f4)
-	MIGSRCDBUSER=$(grep DB_USER /var/www/$MIGSRCDOMAIN/wp-config.php | cut -d"'" -f4)
-	MIGSRCDBPASS=$(grep DB_PASS /var/www/$MIGSRCDOMAIN/wp-config.php | cut -d"'" -f4)
+	MIGSRCDBNAME=$(grep DB_NAME $MIGSRCCONFIG | cut -d"'" -f4)
+	MIGSRCDBUSER=$(grep DB_USER $MIGSRCCONFIG | cut -d"'" -f4)
+	MIGSRCDBPASS=$(grep DB_PASS $MIGSRCCONFIG | cut -d"'" -f4)
 
 	echo -e "\n `date`" &>> $MIGRATELOG
 	echo -e "\n Source Domain = $MIGSRCDOMAIN \n Source DB Name = $MIGSRCDBNAME \n Source MySQL User = $MIGSRCDBUSER \n Source MySQL Password = $MIGSRCDBPASS" | tee -ai $MIGRATELOG
@@ -97,10 +99,16 @@ if [ "$MIGDBEXPORT" == "y" ]
 then
 	# Lets Import MySQL
 	echo -e "\033[34m Fetching Destination DB Name, DB User & DB Password...  \e[0m"
-	rsync -avzh $MIGDESTUSER@$MIGDESTIP:/var/www/$MIGDESTDOMAIN/wp-config.php /tmp/ || SyncError "Unable To Fetch wp-config.php From $MIGDESTDOMAIN"
+	rsync -dmavzh --include "/*" --include "wp-config.php" --exclude "*" $MIGDESTUSER@$MIGDESTIP:/var/www/$MIGDESTDOMAIN/wp-config.php /tmp/ || SyncError "Unable To Fetch wp-config.php From $MIGDESTDOMAIN"
 
 	# Rename wp-config file name to avoid conflict
-	mv /tmp/wp-config.php /tmp/$MIGDESTDOMAIN-wp-config.php
+	if [ -d /tmp/htdocs ]
+	then
+		mv /tmp/htdocs/wp-config.php /tmp/$MIGDESTDOMAIN-wp-config.php
+		rm -rf /tmp/htdocs
+	else
+		mv /tmp/wp-config.php /tmp/$MIGDESTDOMAIN-wp-config.php
+	fi
 
 	# Get Destination Database Name, Username and Password
 	MIGDESTDBNAME=$(grep DB_NAME /tmp/$MIGDESTDOMAIN-wp-config.php | cut -d"'" -f4)
@@ -136,7 +144,7 @@ then
 
 		if [ "$MIGSYNCUPLOAD" == "n" ]
 		then
-			rsync -avzh --exclude="wp-content/uploads/"  /var/www/$MIGSRCDOMAIN/htdocs /var/www/$MIGSRCDOMAIN/backup/$MIGSRCDBNAME.sql $MIGDESTUSER@$MIGDESTIP:/var/www/$MIGDESTDOMAIN/ || SyncError "Unable To Sync $MIGSRCDOMAIN To $MIGDESTDOMAIN"
+			rsync -avzh --exclude="wp-content/uploads/" /var/www/$MIGSRCDOMAIN/htdocs /var/www/$MIGSRCDOMAIN/backup/$MIGSRCDBNAME.sql $MIGDESTUSER@$MIGDESTIP:/var/www/$MIGDESTDOMAIN/ || SyncError "Unable To Sync $MIGSRCDOMAIN To $MIGDESTDOMAIN"
 		else
 			rsync -avzh /var/www/$MIGSRCDOMAIN/htdocs /var/www/$MIGSRCDOMAIN/backup/$MIGSRCDBNAME.sql $MIGDESTUSER@$MIGDESTIP:/var/www/$MIGDESTDOMAIN/ || SyncError "Unable To Sync $MIGSRCDOMAIN To $MIGDESTDOMAIN"
 		fi
