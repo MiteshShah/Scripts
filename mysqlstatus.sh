1a5e26a6c1c96ada1f0e20fda8b24ffae47fda0e
Mitesh Shah <Mitesh.Shah@rtcamp.com>
2013-10-14 12:18:07 +0530

    Use Variable To Define Globally Backup & Live Server:

--- mysqlstatus.sh
+++ mysqlstatus.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+BACKUPSERVER=
 echo "Live Server MySQL Master Status:"
 mysql -e "show master status \G;"
 echo
 echo "Backup Server MySQL Slave Status:"
-ssh root@BACKUP-SERVER 'mysql -e "show slave status \G;"'
+ssh root@$BACKUPSERVER 'mysql -e "show slave status \G;"'
 
 echo
 echo
 echo "Backup Server MySQL Master Status:"
-ssh root@BACKUP-SERVER 'mysql -e "show master status \G;"'
+ssh root@$BACKUPSERVER 'mysql -e "show master status \G;"'
 echo
 echo "Live Server MySQL Slave Status:"
 mysql -e "show slave status \G;"
