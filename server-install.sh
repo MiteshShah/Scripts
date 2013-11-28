59d0daf58f0a7adbd299b8ab400d7fde0c84ecdd
Mitesh Shah <Mitesh.Shah@rtcamp.com>
2013-11-28 19:28:34 +0530

    Install pv

--- server-install.sh
+++ server-install.sh
@@ -5,7 +5,7 @@ apt-get update
 apt-get upgrade
 
 # Install Commonly Used Packages
-apt-get -y install vim screen clamav inotify-tools fail2ban mailutils
+apt-get -y install vim screen clamav inotify-tools fail2ban mailutils pv
 
 # Download MySQL Performance Optimize Tool
 wget -cO /usr/local/bin/tuning-primer.sh https://launchpadlibrarian.net/78745738/tuning-primer.sh
