87530cc048bd7a18a481db5e60ec52459a7203ad
Mitesh Shah <Mitesh.Shah@rtcamp.com>
2013-11-28 13:23:53 +0530

    AWS EV2 AUTOSCALE Change NGINX AS PER NEW H/W

---
+++ nginxoptimise.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sed -i "s/worker_processes.*/worker_processes `cat /proc/cpuinfo | grep processor | wc -l`;/" /etc/nginx/nginx.conf
+
+nginx -t && service nginx reload 
+if [ $? -ne 0 ]
+then
+        echo "$(echo -n "IP Address: "; curl -s http://169.254.169.254/latest/meta-data/public-ipv4; echo; ifconfig)" | mail -s "Unable To Reload Nginx On `hostname`" Mitesh.Shah@rtcamp.com
+fi
