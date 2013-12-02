647907fdc497a28bea2056d25f2a54d643ea1eb0
Mitesh Shah <Mitesh.Shah@rtcamp.com>
2013-12-02 16:20:51 +0530

    Filter Port 25

--- server-install.sh
+++ server-install.sh
@@ -10,3 +10,8 @@ apt-get -y install vim screen clamav inotify-tools fail2ban mailutils pv
 # Download MySQL Performance Optimize Tool
 wget -cO /usr/local/bin/tuning-primer.sh https://launchpadlibrarian.net/78745738/tuning-primer.sh
 chmod u+x /usr/local/bin/tuning-primer.sh
+
+
+# Allow Localhost To Connect Port 25 (Postfix)
+iptables -A INPUT -p tcp -s 127.0.0.0/8 --dport 25 -j ACCEPT
+iptables -A INPUT -p tcp --dport 25 -j DROP
