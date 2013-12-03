ae77c5b95bf32a720f92791d5109567b03194fc8
Mitesh Shah <Mitesh.Shah@rtcamp.com>
2013-12-03 12:10:07 +0530

    Added Custom Prompt PS1

--- server-install.sh
+++ server-install.sh
@@ -15,3 +15,10 @@ chmod u+x /usr/local/bin/tuning-primer.sh
 # Allow Localhost To Connect Port 25 (Postfix)
 iptables -A INPUT -p tcp -s 127.0.0.0/8 --dport 25 -j ACCEPT
 iptables -A INPUT -p tcp --dport 25 -j DROP
+
+# Filter Port 25 On Startup
+sed -i "s/exit.*/# Allow Localhost To Connect Port 25 \(Postfix\)\niptables -A INPUT -p tcp -s 127.0.0.0\/8 --dport 25 -j ACCEPT\niptables -A INPUT -p tcp --dport 25 -j DROP\nexit 0;/" /etc/rc.local
+
+# Custom Prompt PS1
+echo 'PS1="\`if [ \$? = 0 ]; then echo \[\e[37m\]^_^[\u@\H:\w]\\$ \[\e[0m\]; else echo \[\e[31m\]O_O[\u@\H:\w]\\$ \[\e[0m\]; fi\`"' >> /root/.bashrc
+
