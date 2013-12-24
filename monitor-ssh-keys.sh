511d6c34b342bc7bfe6431fb235009a2776c413f
Mitesh Shah <Mitesh.Shah@rtCamp.com>
2013-12-24 12:13:09 +0530

    Update monitor-ssh-keys.sh

--- monitor-ssh-keys.sh
+++ monitor-ssh-keys.sh
@@ -5,7 +5,7 @@
 # Monitor SSH Authorised Keys
 while true
 do
-        inotifywait --exclude .swp -e modify /root/.ssh/authorized_keys /home/*/.ssh/authorized_keys
+        inotifywait --exclude .swp -e modify /root/.ssh/authorized_keys /var/www/.ssh/authorized_keys
 	echo "SSH Authorised Keys Files Modified At [`date`]" &>> /var/log/rtsecure.log
 	
         echo "SSH Authorised Keys Files Modified At `date`" | mail -s "$(hostname -f) SSH Keys Modified" Mitesh.Shah@rtcamp.com 
