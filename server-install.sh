2852b515f16b9dc445c63335bbb8e28595a870a8
Mitesh Shah <Mitesh.Shah@rtcamp.com>
2013-12-27 11:28:36 +0530

    Add Glances Monitor Tool Repository

--- server-install.sh
+++ server-install.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# Add Glances Monitor Tool Repository
+add-apt-repository ppa:arnaud-hartmann/glances-stable
+
 # Start Work With Update
 apt-get update
 apt-get upgrade
 
 # Install Commonly Used Packages
-apt-get -y install vim screen clamav inotify-tools fail2ban mailutils pv htop
+apt-get -y install vim screen clamav inotify-tools fail2ban mailutils pv htop glances
 
 # Download MySQL Performance Optimize Tool
 wget -cO /usr/local/bin/tuning-primer.sh https://launchpadlibrarian.net/78745738/tuning-primer.sh
