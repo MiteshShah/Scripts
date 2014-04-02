631c78341cddb2d6cb28883e0aeedf6a73637f2e
Mitesh Shah <Mitesh.Shah@rtCamp.com>
2014-04-02 11:31:13 +0530

    Install Curl

--- server-install.sh
+++ server-install.sh
@@ -11,7 +11,7 @@ apt-get update
 apt-get upgrade
 
 # Install Commonly Used Packages
-apt-get -y install vim screen clamav inotify-tools fail2ban mailutils pv htop glances
+apt-get -y install vim screen clamav inotify-tools fail2ban mailutils pv htop glances curl
 
 # Download MySQL Performance Optimize Tool
 wget -cO /usr/local/bin/tuning-primer.sh https://launchpadlibrarian.net/78745738/tuning-primer.sh
