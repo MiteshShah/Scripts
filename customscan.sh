67b74d6759db691bfbf3e1d1706b40cdd2e2fafc
gau1991 <gaurav.astikar@rtcamp.com>
2014-06-06 15:07:54 +0530

    Better Message for customscan.sh

--- customscan.sh
+++ customscan.sh
@@ -8,10 +8,10 @@
 is_sunday=$(date +%u)
 
 if [ $is_sunday -eq 7 ];then
-	echo "Today is Sunday, Full Scan Running...."
+	echo "Full clamscan is running...."
 	clamscan --exclude=/proc --exclude=/sys --exclude=/dev -r -i / 
 else
-	echo "Today is not Sunday, Running scan for file modified is last 24 hours"
+	echo "Running clamscan for file modified in last 24 hours...."
 	is_running=$(ps -ef | grep clamscan | grep -v grep | wc -l)
 	if [ $is_running -eq 0 ];then
 		find / -not -wholename '/sys/*' -and -not -wholename '/proc/*' -mtime -1 -type f -print0 | xargs -0 -r clamscan -i 
