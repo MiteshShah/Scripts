d6fc37de3a2bba6191c5f931216ae15173844407
gau1991 <gaurav.astikar@rtcamp.com>
2014-06-06 15:00:55 +0530

    Added Better ClamScan

---
+++ customscan.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#1. if one instance is running, second should not start
+#2. scan the files modified in last 24 hours
+#3 full scan on sunday only
+
+
+
+is_sunday=$(date +%u)
+
+if [ $is_sunday -eq 7 ];then
+	echo "Today is Sunday, Full Scan Running...."
+	clamscan --exclude=/proc --exclude=/sys --exclude=/dev -r -i / 
+else
+	echo "Today is not Sunday, Running scan for file modified is last 24 hours"
+	is_running=$(ps -ef | grep clamscan | grep -v grep | wc -l)
+	if [ $is_running -eq 0 ];then
+		find / -not -wholename '/sys/*' -and -not -wholename '/proc/*' -mtime -1 -type f -print0 | xargs -0 -r clamscan -i 
+	fi
+fi
+
