280fb32c2f04e2f9a42cb3edfc1322755d212c70
gau1991 <gaurav.astikar@rtcamp.com>
2014-07-02 16:44:28 +0530

    Fixed Full bakcup issue

--- backup.sh
+++ backup.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 # Set up some variables for logging
-LOGFILE="/var/log/backup.log"
-DAILYLOGFILE="/var/log/backup.daily.log"
+LOGFILE="/var/log/duplicity/backup.log"
+DAILYLOGFILE="/var/log/duplicity/backup.daily.log"
+FULLBACKLOGFILE="/var/log/duplicity/backup.full.log"
 HOST=`hostname`
 DATE=`date +%Y-%m-%d`
 MAILADDR="sys@rtcamp.com"
+TODAY=$(date +%d%m%Y)
 
 is_running=$(ps -ef | grep duplicity | grep -v grep | wc -l)
+
+if [ ! -d /var/log/duplicity ];then
+    mkdir -p /var/log/duplicity
+fi
 
 if [ $is_running -eq 0 ]; then
     # Clear the old daily log file
@@ -37,7 +43,8 @@ if [ $is_running -eq 0 ]; then
     DEST="s3://s3.amazonaws.com//"
 
     FULL=
-    if [ $(date +%d) -eq 1 ]; then
+    tail -1 ${FULLBACKLOGFILE} | grep ${TODAY}
+    if [ $? -eq 0 && $(date +%d) -eq 1 ]; then
             FULL=full
     fi;
 
@@ -67,8 +74,8 @@ if [ $is_running -eq 0 ]; then
     BACKUPSTATUS=`cat "$DAILYLOGFILE" | grep Errors | awk '{ print $2 }'`
     if [ "$BACKUPSTATUS" != "0" ]; then
 	   cat "$DAILYLOGFILE" | mail -s "Duplicity Backup Log for $HOST - $DATE" $MAILADDR
-    #else
-    #echo No mail | mail -s "Duplicity Backup Log for $HOST - $DATE" $MAILADDR
+    elif [ $FULL == "full"]
+        echo "$(date +%d%m%Y_%T) Full Back Done" >> $FULLBACKLOGFILE
     fi
 
     # Append the daily log file to the main log file
