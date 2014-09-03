b12edb5b6c65b373693842d7714e90834e745641
Manish Songirkar <manishsongirkar36@gmail.com>
2014-09-03 15:33:09 +0530

    Added Nodejs, SASS, Compass, Foundation, Grunt and Bower and there supporting packages

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -40,10 +40,15 @@ clear
 echo "Install Repository For Skype..."
 sudo sh -c  'echo "deb http://archive.canonical.com/ubuntu/ $(lsb_release -sc) partner" >> /etc/apt/sources.list.d/canonical_partner.list' || OwnError "Unable To Add Skype Repository  :("
 
+#NodeJs Repository
+clear
+echo "Install Repository For Shutter..."
+sudo add-apt-repository -y ppa:chris-lea/node.js || OwnError "Unable To NodeJs Repository  :("
+
 #Shutter Repository
 clear
 echo "Install Repository For Shutter..."
-sudo add-apt-repository ppa:shutter/ppa || OwnError "Unable To Add Shutter Repository  :("
+sudo add-apt-repository -y ppa:shutter/ppa || OwnError "Unable To Add Shutter Repository  :("
 
 #Update The Cache
 clear
@@ -54,7 +59,37 @@ sudo apt-get update || OwnError "Updating Cache Failed :("
 #Install Common Softwares
 clear
 echo "Installing Git Vim Filezilla Google-Chrome Skype Oracle-Jdk Opera"
-sudo apt-get -y install git-core openssh-server shutter pv vim vlc curl filezilla google-chrome-stable skype sni-qt sni-qt:i386 libasound2-plugins:i386 openjdk-7-jre icedtea-7-plugin openjdk-7-jdk diodon diodon-plugins ubuntu-restricted-extras p7zip-full p7zip-rar opera nautilus-open-terminal || OwnError "Installation Failed :("
+sudo apt-get -y install git-core openssh-server shutter pv vim vlc curl filezilla google-chrome-stable skype sni-qt sni-qt:i386 libasound2-plugins:i386 openjdk-7-jre icedtea-7-plugin openjdk-7-jdk diodon diodon-plugins ubuntu-restricted-extras p7zip-full p7zip-rar opera nautilus-open-terminal nodejs libcurl4-gnutls-dev libopenssl-ruby libxml2 libxml2-dev libxslt1-dev ruby-dev ruby-sass rubygems ruby1.9.1 || OwnError "Installation Failed :("
+
+#Install SASS
+clear
+echo "Install SASS"
+sudo gem install sass
+
+#Install Compass
+clear
+echo "Install Compass"
+sudo gem install compass
+
+#Install Foundation
+clear
+echo "Install Foundation"
+sudo gem install foundation
+
+#Install SASS
+clear
+echo "Install Zurb Foundation"
+sudo gem install zurb-foundation
+
+#Install Grunt CLI
+clear
+echo "Install Grunt"
+sudo npm install -g grunt-cli
+
+#Install Bower
+clear
+echo "Install Bower"
+sudo npm install -g bower
 
 #Install Netbeans
 clear
