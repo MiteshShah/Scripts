998fbf524d2abc28891e19fa4c8e069f1fc6ffb4
Mitesh Shah <Mitesh.Shah@rtCamp.com>
2014-11-17 18:20:01 +0530

    Delete nginxoptimise.sh

--- nginxoptimise.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sed -i "s/worker_processes.*/worker_processes `cat /proc/cpuinfo | grep processor | wc -l`;/" /etc/nginx/nginx.conf
-
-nginx -t && service nginx reload 
-if [ $? -ne 0 ]
-then
-        echo "$(echo -n "IP Address: "; curl -s http://169.254.169.254/latest/meta-data/public-ipv4; echo; ifconfig)" | mail -s "Unable To Reload Nginx On $(hostname -f)" Mitesh.Shah@rtcamp.com
-fi
